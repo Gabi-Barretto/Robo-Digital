9bb22845ce80861f0e841767c97bd3b71fdef548
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2023-04-02 20:52:31 -0300

    Detalhes Finais

--- Robo Digital - GODOT/Mao.gd
+++ Robo Digital - GODOT/Mao.gd
@@ -1,15 +1,17 @@
 extends Spatial
 
+#iniciando timer e a conexão para realizar o request
 func _ready():
 	$Timer.start()
 	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 	
 func _on_request_completed(result, response_code, headers, body):
+	#recebendo a informação e organizando para movimentar o objeto principal da cena
 	var json = JSON.parse(body.get_string_from_utf8())
 	self.translation = Vector3(float(json.result["x"]), float(json.result["y"]), float(json.result["z"]))
-	#$Mao.translation(Vector3(float(json.result["x"]), float(json.result["y"]), float(json.result["z"])))
+
 	print(json.result)
-	print(json.result["x"])
 	
 func _on_Timer_timeout():
+	#Realiza a requisição a cada 1 segundo
 	$HTTPRequest.request("http://127.0.0.1:3000/mover")
